60ab46cc89b44f563531764b6cd467d3b2d3265a
ikzsl <ikzsl@yandex.ru>
2020-05-30 05:43:22 +0300

    add formik-antd-2

--- server/server.js
+++ server/server.js
@@ -1,26 +1,48 @@
 const express = require('express');
-const path = require('path');
+const cors = require('cors');
 const bodyParser = require('body-parser');
+
+let users = [];
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3012, () => {
-  console.log('API app started');
+  // console.log('API app started');
 });
 
-app.use((req, res, next) => {
-  console.log('Time:', Date.now());
-  next();
-});
-
-app.get('/q', (req, res, next) => {
-  console.log(req.params);
-  res.send('okkkkkkk').end();
-});
+app.use(cors());
+app.use(bodyParser.json());
 
 app.post('/sign-up', (req, res) => {
-  console.log(req.body);
+  const {
+    name, password, email, website, age, skills,
+  } = req.body;
+  if (users.findIndex((user) => email === user.email) !== -1) {
+    // console.log('АЖЫБГА-такой уже есть');
+    // res.send('АЖЫБГА-такой уже есть');
+    throw new Error('Something broke yet again! ');
+  } else {
+    users = [
+      ...users,
+      {
+        name,
+        password,
+        email,
+        website,
+        age,
+        skills,
+      },
+    ];
+  }
+
+  app.use((error, req, res, next) => {
+    res.status(400);
+    res.send('АЖЫБГА-такой уже есть');
+  });
+
+  // console.log(11111111, email, users[0].email, users);
+
   res.send('okkkkkkk').end();
 });
--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {
-  Formik, Form, Field, ErrorMessage,
-} from 'formik';
+import { Formik, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-
+import axios from 'axios';
+import {
+  Form, Input, InputNumber, SubmitButton,
+} from 'formik-antd';
 import './form.scss';
 
 const initialValues = {
@@ -18,8 +19,13 @@ const initialValues = {
 };
 
 const onSubmit = (values) => {
-  console.log(values);
-  alert(JSON.stringify(values, null, 2));
+  // console.log(values);
+  // alert(JSON.stringify(values, null, 2));
+  axios.post('http://localhost:3012/sign-up', values);
+  // .then((response) => console.log(`получил клиент ${response.data}`))
+  // .catch((err) => {
+  // console.log('axios client', err);
+  // });
 };
 
 const validationSchema = Yup.object({
@@ -37,81 +43,60 @@ const validationSchema = Yup.object({
   // acceptTerms: Yup.boolean().required('Нужно  твое согласие'),
 });
 const submitForm = () => (
-  // console.log('form values', formik.values);
-  // console.log('form errors', formik.errors);
-  // console.log('visited fields', formik.touched);
-
-  // action="http://localhost:3012/sign-up"
-  // method="POST"
   <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
     <Form className="form">
       <div>
-        <label htmlFor="name">
-          name
-          <ErrorMessage
-            name="name"
-            className="error"
-            render={(msg) => <span className="error">{msg}</span>}
-          />
-          <Field id="name" type="text" name="name" />
-        </label>
+        <label htmlFor="name">name</label>
+        <Form.Item name="name">
+          <Input id="name" type="text" name="name" />
+        </Form.Item>
       </div>
       <div>
-        <label htmlFor="pwd">
-          Password:
-          <ErrorMessage name="password" render={(msg) => <span className="error">{msg}</span>} />
-          <Field id="pwd" type="password" name="password" />
-        </label>
+        <label htmlFor="pwd">password: </label>
+        <ErrorMessage name="password">{(msg) => <span className="error">{msg}</span>}</ErrorMessage>
+        <Input.Password id="pwd" type="password" name="password" />
       </div>
       <div>
         <label htmlFor="repwd">passwordConfirmation:</label>
-        <ErrorMessage
-          name="passwordConfirmation"
-          render={(msg) => <span className="error">{msg}</span>}
-        />
-        <Field id="repwd" type="password" name="passwordConfirmation" />
+        <ErrorMessage name="passwordConfirmation">
+          {(msg) => <span className="error">{msg}</span>}
+        </ErrorMessage>
+        <Input.Password id="repwd" type="password" name="passwordConfirmation" />
       </div>
       <div>
-        <label htmlFor="email">
-          email
-          <ErrorMessage name="email" render={(msg) => <span className="error">{msg}</span>} />
-          <Field id="email" type="email" name="email" />
-        </label>
+        <label htmlFor="email">email </label>
+        <ErrorMessage name="email">{(msg) => <span className="error">{msg}</span>}</ErrorMessage>
+        <Input id="email" type="email" name="email" />
       </div>
 
       <div>
-        <label htmlFor="site">
-          website
-          <Field id="site" type="text" name="website" />
-        </label>
+        <label htmlFor="site">website </label>
+        <Input id="site" type="text" name="website" />
       </div>
 
       <div>
-        <label htmlFor="age">
-          age
-          <ErrorMessage name="age" render={(msg) => <span className="error">{msg}</span>} />
-          <Field id="age" type="text" name="age" />
-        </label>
+        <label htmlFor="age">age </label>
+        <ErrorMessage name="age">{(msg) => <span className="error">{msg}</span>}</ErrorMessage>
+        <InputNumber id="age" name="age" min={18} />
       </div>
 
       <div>
-        <label htmlFor="skills">
-          skills
-          <Field id="skills" type="text" name="skills" />
-        </label>
+        <label htmlFor="skills">skills </label>
+        <Input id="skills" type="text" name="skills" />
       </div>
 
       <div>
-        <label htmlFor="terms">
-          acceptTerms
-          <ErrorMessage name="acceptTerms" render={(msg) => <span className="error">{msg}</span>} />
-          <Field id="terms" type="checkbox" name="acceptTerms" />
-        </label>
+        <label htmlFor="terms">acceptTerms </label>
+        <ErrorMessage name="acceptTerms">
+          {(msg) => <span className="error">{msg}</span>}
+        </ErrorMessage>
+        <Field id="terms" type="checkbox" name="acceptTerms" />
       </div>
 
       <div>
         <button type="submit">Submit</button>
       </div>
+      <SubmitButton disabled={false} />
     </Form>
   </Formik>
 );
