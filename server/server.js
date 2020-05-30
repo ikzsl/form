8746f928b3387d5f98c588e1256dbab571b4ddf6
ikzsl <ikzsl@yandex.ru>
2020-05-30 23:52:42 +0300

    add server message

--- server/server.js
+++ server/server.js
@@ -16,12 +16,19 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/sign-up', (req, res) => {
-  const {
-    name, password, email, website, age, skills,
-  } = req.body;
-  if (users.findIndex((user) => email === user.email) !== -1) {
-    // console.log('АЖЫБГА-такой уже есть');
+  const { name, password, email, website, age, skills } = req.body;
+  console.log(users.findIndex((user) => email === user.email));
+  if (
+    users.findIndex((user) => {
+      console.log('АЖЫБГА-такой уже есть', email, user.email);
+      return email === user.email;
+    }) > -1
+  ) {
     // res.send('АЖЫБГА-такой уже есть');
+    app.use((error, req, res, next) => {
+      res.status(400);
+      res.send('Пользователь с такой почтой уже есть').end();
+    });
     throw new Error('Something broke yet again! ');
   } else {
     users = [
@@ -35,14 +42,10 @@ app.post('/sign-up', (req, res) => {
         skills,
       },
     ];
+    res.status(200);
+    res.send('Пользователь успешно зарегистрирован').end();
   }
 
-  app.use((error, req, res, next) => {
-    res.status(400);
-    res.send('АЖЫБГА-такой уже есть');
-  });
+  // console.log(11111111, email, email, users);
 
-  // console.log(11111111, email, users[0].email, users);
-
-  res.send('okkkkkkk').end();
 });
--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,103 +1,152 @@
 import React from 'react';
-import { Formik, Field, ErrorMessage } from 'formik';
+import { Formik } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 import {
-  Form, Input, InputNumber, SubmitButton,
+  Form, Input, InputNumber, Table, AddRowButton, Checkbox,
 } from 'formik-antd';
 import './form.scss';
 
-const initialValues = {
-  name: '',
-  password: '',
-  passwordConfirmation: '',
-  email: '',
-  website: '',
-  age: '',
-  skills: [],
-  acceptTerms: true,
-};
+class submitForm extends React.Component {
+  state = {
+    errorMessage: null,
+    successMessage: null,
+  };
 
-const onSubmit = (values) => {
-  // console.log(values);
-  // alert(JSON.stringify(values, null, 2));
-  axios.post('http://localhost:3012/sign-up', values);
-  // .then((response) => console.log(`получил клиент ${response.data}`))
-  // .catch((err) => {
-  // console.log('axios client', err);
-  // });
-};
+  initialValues = {
+    name: '',
+    password: '',
+    passwordConfirmation: '',
+    email: '',
+    website: '',
+    age: '',
+    skills: [],
+    acceptTerms: false,
+  };
 
-const validationSchema = Yup.object({
-  name: Yup.string().max(50, 'Слишком длинно - не более 50 символов').required('Имя обязательно'),
-  password: Yup.string().required('Пароль нужен'),
-  passwordConfirmation: Yup.string()
-    .oneOf([Yup.ref('password'), null], 'Надо точь-в-точь как пароль')
-    .required('Обязательно'),
-  email: Yup.string().email('Неправильная почта').required('Почту, пожалуйста'),
-  age: Yup.number()
-    .min(18, 'Юнцам тут не место')
-    .max(65, 'Займись лучше внуками, дедуля')
-    .required('Сколько тебе лет?'),
-  skills: Yup.array(),
-  // acceptTerms: Yup.boolean().required('Нужно  твое согласие'),
-});
-const submitForm = () => (
-  <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
-    <Form className="form">
-      <div>
-        <label htmlFor="name">name</label>
-        <Form.Item name="name">
-          <Input id="name" type="text" name="name" />
-        </Form.Item>
-      </div>
-      <div>
-        <label htmlFor="pwd">password: </label>
-        <ErrorMessage name="password">{(msg) => <span className="error">{msg}</span>}</ErrorMessage>
-        <Input.Password id="pwd" type="password" name="password" />
-      </div>
-      <div>
-        <label htmlFor="repwd">passwordConfirmation:</label>
-        <ErrorMessage name="passwordConfirmation">
-          {(msg) => <span className="error">{msg}</span>}
-        </ErrorMessage>
-        <Input.Password id="repwd" type="password" name="passwordConfirmation" />
-      </div>
-      <div>
-        <label htmlFor="email">email </label>
-        <ErrorMessage name="email">{(msg) => <span className="error">{msg}</span>}</ErrorMessage>
-        <Input id="email" type="email" name="email" />
-      </div>
+  validationSchema = Yup.object({
+    name: Yup.string().max(50, 'Слишком длинно - не более 50 символов').required('Имя обязательно'),
+    password: Yup.string().required('Пароль нужен'),
+    passwordConfirmation: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Надо точь-в-точь как пароль')
+      .required('Обязательно'),
+    email: Yup.string().email('Неправильная почта').required('Почту, пожалуйста'),
+    age: Yup.number()
+      .min(18, 'Юнцам тут не место')
+      .max(65, 'Займись лучше внуками, дедуля')
+      .required('Сколько тебе лет?'),
+    skills: Yup.array(),
+    acceptTerms: Yup.bool().oneOf([true], 'Нужно  твое согласие'),
+  });
 
-      <div>
-        <label htmlFor="site">website </label>
-        <Input id="site" type="text" name="website" />
-      </div>
+  onSubmit = (values) => {
+    // console.log(values);
+    // alert(JSON.stringify(values, null, 2));
+    axios
+      .post('http://localhost:3012/sign-up', values)
+      .then((response) => {
+        // console.log(`получил клиент ${response.data}`);
+        this.setState({ errorMessage: null, successMessage: response.data });
+      })
+      .catch((err) => {
+        // console.log('axios client', err.response, err.response.data);
+        this.setState({ errorMessage: err.response.data, successMessage: null });
+      });
+  };
 
-      <div>
-        <label htmlFor="age">age </label>
-        <ErrorMessage name="age">{(msg) => <span className="error">{msg}</span>}</ErrorMessage>
-        <InputNumber id="age" name="age" min={18} />
-      </div>
+  render() {
+    const { successMessage, errorMessage } = this.state;
+    return (
+      <Formik
+        initialValues={this.initialValues}
+        validationSchema={this.validationSchema}
+        onSubmit={this.onSubmit}
+      >
+        <Form className="form">
+          <div>
+            <label htmlFor="name">Имя</label>
+            <Form.Item name="name">
+              <Input id="name" name="name" placeholder="Иван" />
+            </Form.Item>
+          </div>
+          <div>
+            <label htmlFor="pwd">Пароль </label>
+            <Form.Item name="password">
+              <Input.Password id="pwd" name="password" placeholder="bu@UYvj#lkfhk!))nkjshd" />
+            </Form.Item>
+          </div>
+          <div>
+            <label htmlFor="repwd">Повторите пароль</label>
+            <Form.Item name="passwordConfirmation">
+              <Input.Password
+                id="repwd"
+                name="passwordConfirmation"
+                placeholder="bu@UYvj#lkfhk!))nkjshd"
+              />
+            </Form.Item>
+          </div>
+          <div>
+            <label htmlFor="email">Электропочта </label>
+            <span className="error">{errorMessage}</span>
+            <Form.Item name="email">
+              <Input id="email" name="email" placeholder="ivan@mail.ru" />
+            </Form.Item>
+          </div>
 
-      <div>
-        <label htmlFor="skills">skills </label>
-        <Input id="skills" type="text" name="skills" />
-      </div>
+          <div>
+            <label htmlFor="site">Ваш сайт </label>
+            <Input id="site" name="website" placeholder="www.ivan.ru" />
+          </div>
 
-      <div>
-        <label htmlFor="terms">acceptTerms </label>
-        <ErrorMessage name="acceptTerms">
-          {(msg) => <span className="error">{msg}</span>}
-        </ErrorMessage>
-        <Field id="terms" type="checkbox" name="acceptTerms" />
-      </div>
+          <div>
+            <label htmlFor="age">Возраст </label>
+            <Form.Item name="age">
+              <InputNumber id="age" name="age" placeholder="Возраст" />
+            </Form.Item>
+          </div>
 
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-      <SubmitButton disabled={false} />
-    </Form>
-  </Formik>
-);
+          {/* <div>
+            <label htmlFor="skills">Навыки </label>
+            <Field id="skills" type="text" name="skills" placeholder="Навыки" />
+          </div> */}
+
+          <AddRowButton
+            name="skills"
+            createNewRow={(text, record) => ({
+              name: record,
+            })}
+          >
+            Add
+          </AddRowButton>
+
+          <Table
+            name="skills"
+            rowKey={(row) => `${row.id}`}
+            size="small"
+            pagination={false}
+            columns={[
+              {
+                title: 'Name',
+                key: 'name',
+                render: (text, record, i) => <Input name={`skills.${i}.name`} />,
+              },
+            ]}
+          />
+
+          <div>
+            <Form.Item name="acceptTerms">
+              <Checkbox id="terms" name="acceptTerms" />
+              <label htmlFor="terms"> Согласен с условиями </label>
+            </Form.Item>
+          </div>
+
+          <div>
+            <button type="submit">Зарегистрироваться</button>
+            <span className="success">{successMessage}</span>
+          </div>
+        </Form>
+      </Formik>
+    );
+  }
+}
 export default submitForm;
